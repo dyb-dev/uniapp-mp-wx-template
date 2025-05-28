826a6b812ab8dc9267037ed98ef678509320b3d5
dyb-dev <2737993954@qq.com>
2025-05-28 23:58:37 +0800

    ✨ feat: 新增相关工具函数

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,12 +2,14 @@
  * @Author: dyb-dev
  * @Date: 2024-10-08 12:02:08
  * @LastEditors: dyb-dev
- * @LastEditTime: 2024-11-16 02:11:10
+ * @LastEditTime: 2025-05-28 23:47:42
  * @FilePath: /uniapp-mp-wx-template/src/hooks/index.ts
  * @Description: hook模块
  */
 
-/** 导出分享相关hook */
-export * from "./share"
 /** 导出分页相关hook */
 export * from "./pagination"
+/** 导出分享相关hook */
+export * from "./share"
+/** 导出时间控制相关hooks函数 */
+export * from "./timer"
--- src/utils/data/index.ts
+++ src/utils/data/index.ts
@@ -2,7 +2,7 @@
  * @Author: dyb-dev
  * @Date: 2024-11-05 00:55:26
  * @LastEditors: dyb-dev
- * @LastEditTime: 2024-11-05 11:55:40
+ * @LastEditTime: 2025-05-28 23:48:40
  * @FilePath: /uniapp-mp-wx-template/src/utils/data/index.ts
  * @Description: 数据处理相关工具函数
  */
@@ -11,13 +11,16 @@
  * FUN: 深度克隆
  * - 支持对象、数组、Map、Set、Date、RegExp 等类型的深度克隆
  *
+ * 注意：
+ * - 为了能够在克隆目标对象内部处理含有相同引用的对象，因此不支持处理循环引用问题，书写代码时需注意
+ *
  * @author dyb-dev
- * @date 05/11/2024/  11:45:21
+ * @date 28/05/2025/  23:10:22
+ * @template T
  * @param {T} obj 要克隆的对象
- * @param {*} [cache=new WeakMap()] 缓存对象
  * @returns {*}  {T} 克隆后的对象
  */
-const deepClone = <T = any>(obj: T, cache = new WeakMap()): T => {
+export const deepClone = <T = unknown>(obj: T): T => {
 
     // 如果 obj 是 null 或者不是对象（即基本类型），直接返回该值，因为这些值不需要深度克隆
     if (obj === null || typeof obj !== "object") {
@@ -26,75 +29,153 @@ const deepClone = <T = any>(obj: T, cache = new WeakMap()): T => {
 
     }
 
-    // 如果 cache 中已经存在 obj，则直接返回缓存中的克隆对象，防止循环引用导致的无限递归
-    if (cache.has(obj)) {
-
-        return cache.get(obj)
-
-    }
-
-    let clone
+    let _clone
 
     // 如果 obj 是 Date 类型，创建一个新的 Date 实例并赋值给 clone
     if (obj instanceof Date) {
 
-        clone = new Date(obj.getTime())
+        _clone = new Date(obj.getTime())
 
     }
-    // 如果 obj 是 RegExp 类型，创建一个新的正则表达式实例并赋值给 clone
     else if (obj instanceof RegExp) {
 
-        clone = new RegExp(obj)
+        _clone = new RegExp(obj)
 
     }
-    // 如果 obj 是 Map 类型，则使用递归来克隆 Map 中的每一个键值对，并创建新的 Map 实例
     else if (obj instanceof Map) {
 
-        // 使用 Array.from 遍历 obj 中的每一个键值对，并递归地克隆值
-        clone = new Map(Array.from(obj, ([key, value]) => [key, deepClone(value, cache)]))
+        _clone = new Map(Array.from(obj, ([key, value]) => [key, deepClone(value)]))
 
     }
-    // 如果 obj 是 Set 类型，则使用递归来克隆 Set 中的每一个值，并创建新的 Set 实例
     else if (obj instanceof Set) {
 
-        // 使用 Array.from 遍历 obj 中的每一个值，并递归地克隆
-        clone = new Set(Array.from(obj, value => deepClone(value, cache)))
+        _clone = new Set(Array.from(obj, value => deepClone(value)))
 
     }
-    // 如果 obj 是数组，则使用 map 方法递归地克隆数组中的每一个元素
     else if (Array.isArray(obj)) {
 
-        clone = obj.map(value => deepClone(value, cache))
+        _clone = obj.map(value => deepClone(value))
 
     }
-    // 如果 obj 是普通对象（即 [object Object]），则创建一个新的对象并递归地克隆其属性
     else if (Object.prototype.toString.call(obj) === "[object Object]") {
 
         // 创建一个新的对象，保持原始对象的原型链
-        clone = Object.create(Object.getPrototypeOf(obj))
-        // 将 obj 存入缓存，防止循环引用
-        cache.set(obj, clone)
+        _clone = Object.create(Object.getPrototypeOf(obj))
         // 使用 Object.entries 遍历对象的每一个键值对，并递归地克隆每个属性
         for (const [key, value] of Object.entries(obj)) {
 
-            clone[key] = deepClone(value, cache)
+            _clone[key] = deepClone(value)
 
         }
 
     }
-    // 如果 obj 是其他类型（例如函数），直接创建一个浅拷贝
     else {
 
-        clone = Object.assign({}, obj)
+        _clone = Object.assign({}, obj)
 
     }
 
-    // 在缓存中记录克隆的对象，防止重复克隆
-    cache.set(obj, clone)
-
-    // 返回深度克隆后的对象
-    return clone
+    return _clone
 
 }
 
-export { deepClone }
+/**
+ * FUN: 检查是否是一个字面量对象 {}
+ *
+ * @author dyb-dev
+ * @date 28/05/2025/  23:12:40
+ * @param {*} obj 目标对象
+ * @returns {*}  {boolean} 结果
+ */
+export const isObject = (obj: unknown): boolean => Object.prototype.toString.call(obj) === "[object Object]"
+
+/** 字段名类型 */
+type TFieldName = string | number | symbol
+
+/** 字段映射类型 */
+type TFieldMapping<T> = {
+    [newField in TFieldName]: keyof T | ((key: keyof T) => boolean)
+}
+
+/**
+ * FUN: 替换字段名
+ * - 支持对象、数组，并返回新的引用
+ *
+ * @author dyb-dev
+ * @date 28/05/2025/  23:17:39
+ * @template T
+ * @param {(T | T[])} data 目标对象、数组
+ * @param {TFieldMapping<T>} fieldMapping 字段名映射关系对象 新字段名: 被替换字段名
+ * @returns {*}  {(T | T[])} 结果
+ */
+export const replaceFieldNames = <T>(data: T | T[], fieldMapping: TFieldMapping<T>): T | T[] => {
+
+    // 深度克隆传入的数据，避免修改原数据
+    const _clonedData = deepClone(data)
+    // 字段映射key数组
+    const _fieldMappingKeys = Object.keys(fieldMapping)
+
+    // 递归替换字段名的函数
+    const _replaceFieldNames = (targetData: any): any => {
+
+        // 如果是数组，则递归处理每个元素
+        if (Array.isArray(targetData)) {
+
+            return targetData.map(_item => _replaceFieldNames(_item))
+
+        }
+
+        // 如果是对象，则递归处理每个属性
+        if (isObject(targetData)) {
+
+            Object.keys(targetData).forEach(key => {
+
+                const _key = key as keyof T
+                const _fieldMappingKey = _fieldMappingKeys.find(_field => {
+
+                    const _mappingValue = fieldMapping[_field]
+                    return typeof _mappingValue === "function" ? _mappingValue(_key) : _mappingValue === _key
+
+                })
+
+                // 新的字段名
+                const _newKey = _fieldMappingKey || key
+
+                // 如果新字段名和原字段名不同，进行替换
+                if (_newKey !== key) {
+
+                    // 新字段赋值
+                    targetData[_newKey] = targetData[key]
+                    // 删除旧字段
+                    delete targetData[key]
+
+                }
+                // 递归处理字段值
+                targetData[_newKey] = _replaceFieldNames(targetData[_newKey])
+
+            })
+
+        }
+
+        return targetData
+
+    }
+
+    return _replaceFieldNames(_clonedData)
+
+}
+
+/**
+ * FUN: 根据分隔符分割字符串
+ *
+ * @author dyb-dev
+ * @date 28/05/2025/  23:24:19
+ * @param {string} str 需要分割的字符串
+ * @param {string} delimiter 分隔符
+ * @returns {*}  {string[]} 结果数组
+ */
+export const splitStringByDelimiter = (str: string, delimiter: string): string[] => {
+
+    return delimiter ? str.split(new RegExp(`(?<=${delimiter})|(?=${delimiter})`, "g")) : [str]
+
+}
